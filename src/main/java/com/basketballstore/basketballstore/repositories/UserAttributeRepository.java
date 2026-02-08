59b69e2cd689249389dd3455f57eff5346bd2e70
kauevera <kaueveras02@gmail.com>
2026-02-08 13:21:54 -0300

    feat: removes user attribute layers

--- src/main/java/com/basketballstore/basketballstore/repositories/UserAttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.basketballstore.basketballstore.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.basketballstore.basketballstore.models.UserAttribute;
-
-public interface UserAttributeRepository extends JpaRepository<UserAttribute, Long> {
-    
-}
