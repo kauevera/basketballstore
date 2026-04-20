1e31a63cbf06c5422421f88a0ebbd810edd92533
kauevera <kaueveras02@gmail.com>
2026-04-20 20:26:24 -0300

    feat: adds a layer for frontend archives

--- src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
-INSERT INTO users_table (name, email, password) VALUES ('kaue', 'teste@gmail.com', 'kaue0901');
