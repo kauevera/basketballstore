3c966dd9077bfe6ec9234f08b643a6a1931ba282
kauevera <kaueveras02@gmail.com>
2026-04-20 20:24:51 -0300

    feat: adds products screen stylization

---
+++ frontend/static/js/pages_redirect.js
@@ -0,0 +1,7 @@
+function redirectLogin() {
+    window.location.href = "../../templates/auth/login.html"
+}
+
+function redirectDashboard() {
+    window.location.href = "../../templates/products/dashboard.html"
+}
