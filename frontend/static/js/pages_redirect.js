048f13cbd30ca7100b443e75116226d1aedf3cb1
kauevera <kaueveras02@gmail.com>
2026-04-20 21:11:40 -0300

    feat: adds register screen stylization

--- frontend/static/js/pages_redirect.js
+++ frontend/static/js/pages_redirect.js
@@ -2,6 +2,10 @@ function redirectLogin() {
     window.location.href = "../../templates/auth/login.html"
 }
 
+function redirectRegister() {
+    window.location.href = "../../templates/auth/register.html"
+}
+
 function redirectDashboard() {
     window.location.href = "../../templates/products/dashboard.html"
 }
